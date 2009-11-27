04cc8c3b230a223961c69cd4cedd542974de07b1
agcc <agcc@agcc-laptop.(none)>
2009-11-27 13:53:21 -0600

    new version uses property file now for project compiles correctly

--- IDE.java
+++ IDE.java
@@ -1,9 +1,17 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
+
+
+abstract class ParentRunnable implements Runnable {
+	IDE parent;
+	public ParentRunnable(IDE parent) {
+		this.parent = parent;
+	}
+	abstract public void run();
+}
 
 public class IDE {
 
@@ -11,19 +19,42 @@ public class IDE {
 	DefaultListModel projectFiles = new DefaultListModel();
 	JList projectList = null;
 
+	DefaultListModel classpathModel = new DefaultListModel();
+	JList classPathList = null;
+
 	public void loadProjectFile(File f) {
 		try {
 			projectFiles.clear();
-			BufferedReader inf = new BufferedReader(new FileReader(f));
-			while(true) {
-				String line = inf.readLine();
-				if (line==null) break;
-				projectFiles.addElement(line);
+			Properties props = new Properties();
+			FileInputStream inf = new FileInputStream(f);
+			props.load(inf);
+			int count = Integer.parseInt(
+				props.getProperty("sourcefiles"));
+			for(int i=0;i<count;i++) {
+				projectFiles.addElement(
+					props.getProperty("f"+i)
+				);
 			}
-			inf.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void showCompileResults(String s) {
+		System.out.println(s);
+	}
+
+	public void compileFile(String filename) {
+		System.out.println("compiling " + filename);
+		String[] args = new String[4];
+		args[0] = "-verbose";
+		args[1] = "-d";
+		args[2] = "./classes";
+		args[3] = filename;
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		com.sun.tools.javac.Main.compile(args, pw);
+		System.out.println(sw.toString());
 	}
 
 	public String loadFile(File f) {
@@ -52,7 +83,7 @@ public class IDE {
 		JButton open = new JButton("open");
 		open.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser fc = new JFileChooser();
+				JFileChooser fc = new JFileChooser(".");
 				int ret = fc.showOpenDialog(mf);
 				if (ret==JFileChooser.APPROVE_OPTION) {
 					File inf = fc.getSelectedFile();
@@ -64,6 +95,20 @@ public class IDE {
 		JButton save = new JButton("save");
 		top.add(save);
 		JButton compile = new JButton("compile");
+		final ParentRunnable runner = new ParentRunnable(this) {
+			public void run() {
+					int idx = projectList.getSelectedIndex();
+					String filename = (String)projectFiles
+					.get(idx);
+					compileFile(filename);
+				}
+		};
+	
+		compile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.invokeLater(runner);
+			}
+		});
 		top.add(compile);
 		mf.getContentPane().add(top, BorderLayout.NORTH);
 		JPanel left = new JPanel();
